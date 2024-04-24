14c5724809dd25b5fc8da265204e64869121acbb
MADUBUIKE Nelson Onyedikachi <110107766+kachi72@users.noreply.github.com>
2024-04-24 16:02:43 +0000

    added html file and css

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask,  redirect, url_for, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/")
 def display():
-    return "Hello Kachi"
+  return render_template("index.html")
+
+
+@app.route("/next")
+def next():
+  return redirect(url_for('display'))
+
+
+@app.route("/test")
+def test():
+  return "This is the kachi function"
+
+
+@app.route("/second")
+def second():
+  return redirect(url_for('test'))
 
 
 if __name__ == "__main__":
