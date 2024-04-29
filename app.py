f8a4f551222c609e4e7ab0da0fa36d0d5c6c9472
MADUBUIKE Nelson Onyedikachi <110107766+kachi72@users.noreply.github.com>
2024-04-29 10:25:28 +0000

    added dynamic data and API

--- app.py
+++ app.py
@@ -1,26 +1,46 @@
-from flask import Flask,  redirect, url_for, render_template
+from flask import Flask,  redirect, url_for, render_template,jsonify
 
 app = Flask(__name__)
 
 
+JOBS = [
+  {
+    'id': 1,
+    'title': 'Data Scientist',
+    'location': 'Bengaluru, India',
+    'salary': 'Rs. 10,00,000'
+  },
+  {
+    'id': 2,
+    'title': 'Data Analyst',
+    'location': 'Delhi, India',
+    'salary': 'Rs. 15,00,000'
+  },
+  {
+    'id': 3,
+    'title': 'Frontend Engineer',
+    'location': 'Lagos, Nigeria',
+    'salary': 'Ngn. 200,000,00'
+  },
+  {
+    'id': 4,
+    'title': 'Backend Engineer',
+    'location': 'Remote',
+    'salary': '$. 200,000'
+  }
+]
+
 @app.route("/")
 def display():
-  return render_template("index.html")
+  return render_template("index.html", jobs=JOBS)
 
-
-@app.route("/next")
+@app.route("/redirect")
 def next():
   return redirect(url_for('display'))
 
-
-@app.route("/test")
-def test():
-  return "This is the kachi function"
-
-
-@app.route("/second")
-def second():
-  return redirect(url_for('test'))
+@app.route("/api/jobs")
+def list_jobs():
+  return jsonify(JOBS)
 
 
 if __name__ == "__main__":
